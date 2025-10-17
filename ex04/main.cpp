ce1ca9b38ee79032c579161f8fd006d310dfa96a
Killian0b101010 <teixeira.killian44600@gmail.com>
2025-10-17 02:48:56 +0200

    mise a la norme mm si pas de norme (c plus propre)

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 22:57:36 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/10/17 01:53:39 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/10/17 02:46:10 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,42 +21,59 @@ std:: istream::pos_type size_file(std::string &file)
   return infile.tellg();
 }
 
+int ft_error(char **argv)
+{
+    std::ifstream inf(argv[1]);
+    if(!inf)
+    {
+      std::cout << "Error cannot open file : " << argv[1] << std::endl;
+      return(1);
+    }
+    std::string file = argv[1];
+    if(size_file(file) <= 0)
+    {
+        std::cout << "Error : File " << "[" << argv[1] << "]" << " is empty" << std::endl;
+        return(1);
+    }
+    return(0);
+}
+
+void ft_find_and_replace(std::ifstream& inf,std::string& line,std::string& s1,
+    std::string& s2,std::ofstream& out)
+{
+  while(std::getline(inf,line))    
+  {
+    size_t pos = line.find(s1);
+    while((pos = line.find(s1,pos)) != std::string::npos)
+    {
+      line = line.substr(0,pos) + s2 + line.substr(pos + s1.length());
+      pos = line.find(s1,pos + s2.length());
+    }
+    out << line << std::endl;
+  }
+}
+
 int main(int ac, char **argv)
 {
   if(ac == 4)
   {
     std::ifstream inf(argv[1]);
-    if(!inf)
-    {
-      std::cout << "Error cannot open file : " << argv[1] << std::endl;
-      return(0);
-    }
     std::string s1;
     std::string s2;
-    std::string file = argv[1];
-    if(size_file(file) <= 0)
-    {
-        std::cout << "Error : File " << "[" << argv[1] << "]" << " is empty" << std::endl;
-        return(0);
-    }
     s1 = argv[2];
     s2 = argv[3];
     std::string line;
     std::ofstream out((std::string(argv[1]) + ".replace").c_str());
-    size_t pos = line.find(s1);
-    while(std::getline(inf,line))    
-    {
-      size_t pos = line.find(s1);
-      while((pos = line.find(s1,pos)) != std::string::npos)
-      {
-          line = line.substr(0,pos) + s2 + line.substr(pos + s1.length());
-          pos = line.find(s1,pos + s2.length());
-      }
-      out << line << std::endl;
-    }
+
+    if(ft_error(argv))
+        return(0);
+    ft_find_and_replace(inf,line,s1,s2,out);
     return(0);
   }
-  std::cout << "Few or less arguments" << std::endl;
+  if(ac > 4)
+    std::cout << "Error : " << ac << " > 4" << std::endl;
+  else 
+    std::cout << "Error : " << ac << " < 4" << std::endl;
   return(0);
 }
 
